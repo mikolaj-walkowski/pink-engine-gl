7b608562cc5f379f52c249ce4eaf23337a2ccdc7
Mikołaj Walkowski <mikolaj.walkowski.00@gmail.com>
2023-01-13 23:15:32 +0100

    Added a "moved" shape pointer to Rigidbody

--- src/utils.cpp
+++ src/utils.cpp
@@ -72,16 +72,18 @@ void utils::nvidia::setupContext(nvvk::Context* c, std::vector<utils::ExtensionL
 
 }
 
-ps::Object utils::objectCreate(kln::motor m, ps::pp::BodyType bt_type, std::string meshName, ps::pp::BaseShape* shape) {
+ps::Object utils::objectCreate(kln::motor m, ps::pp::BodyType bt_type, std::string meshName, ps::pp::BaseShape* shape, ps::pp::BaseShape* moved) {
     m.normalize();
+    
 
-    ps::pp::Rigidbody rb = { m, ~m(kln::line(0,0,0,0,0,0)), kln::uMotor(), kln::line(),kln::line(0,0,0,0,0,0), kln::origin(), bt_type, shape };
+    ps::pp::Rigidbody rb = { m, ~m(kln::line(0,0,0,0,0,0)), kln::uMotor(), kln::line(),kln::line(0,0,0,0,0,0), kln::origin(), bt_type, shape, moved};
+    rb.moved->move(rb.M, rb.shape);
 
     ps::Object out = {};
 
     out.id = newID();
     out.rigidbody = rb;
-
+    
     auto& names = ps::pg::ObjLibrary::getObjLibrary().m_objectNames;
     auto& name = std::find(names.begin(), names.end(), meshName);
     out.mesh = ps::pg::ObjLibrary::getObjLibrary().GetMesh(name != names.end() ? meshName : ps::pp::shapeName[shape->type]);
@@ -92,5 +94,6 @@ ps::Object utils::objectCreate(kln::motor m, ps::pp::BodyType bt_type, std::stri
 void utils::objectDestroy(ps::Object* ob) {
     if (ob->rigidbody.shape != NULL) {
         delete ob->rigidbody.shape;
+        delete ob->rigidbody.moved;
     }
 }
